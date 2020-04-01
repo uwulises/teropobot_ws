73316b6ff07dbae4d3f36c1c3c35d17cf05bfb18
uwulises <ulises.campodonico@ug.uchile.cl>
2020-03-31 22:07:42 -0300

    nitido

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -1,5 +1,5 @@
 # generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/uwulises/teropobot_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/uwulises/teropobot_ws/devel;/home/uwulises/uchile_ws/ros/bender/high_ws/devel;/home/uwulises/uchile_ws/ros/bender/soft_ws/devel;/home/uwulises/uchile_ws/ros/bender/base_ws/devel;/home/uwulises/uchile_ws/ros/bender/forks_ws/devel;/opt/ros/melodic".split(';') if "/home/uwulises/teropobot_ws/devel;/home/uwulises/uchile_ws/ros/bender/high_ws/devel;/home/uwulises/uchile_ws/ros/bender/soft_ws/devel;/home/uwulises/uchile_ws/ros/bender/base_ws/devel;/home/uwulises/uchile_ws/ros/bender/forks_ws/devel;/opt/ros/melodic" != "" else []
+source_root_dir = '/home/uwulises/teropobot_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/uwulises/teropobot_ws/devel;/home/uwulises/uchile_ws/ros/bender/high_ws/devel;/home/uwulises/uchile_ws/ros/bender/soft_ws/devel;/home/uwulises/uchile_ws/ros/bender/base_ws/devel;/home/uwulises/uchile_ws/ros/bender/forks_ws/devel;/opt/ros/melodic'.split(';') if '/home/uwulises/teropobot_ws/devel;/home/uwulises/uchile_ws/ros/bender/high_ws/devel;/home/uwulises/uchile_ws/ros/bender/soft_ws/devel;/home/uwulises/uchile_ws/ros/bender/base_ws/devel;/home/uwulises/uchile_ws/ros/bender/forks_ws/devel;/opt/ros/melodic' != '' else []
